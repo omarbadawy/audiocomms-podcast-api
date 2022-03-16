62ae970ebee6e3824c5dbd877929eead8baa0500
AhmedMaher015 <ahmededu015@gmail.com>
2022-03-16 16:17:17 +0200

    remove hi

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -64,7 +64,6 @@ const sendErrorProd = (err, req, res) => {
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-    console.log('hi')
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, req, res)
     } else if (process.env.NODE_ENV === 'production') {
