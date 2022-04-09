64754103b7cd55903bc647cb519216389da38625
AhmedMaher015 <ahmededu015@gmail.com>
2022-04-10 00:14:24 +0200

    Add isActivated in joinRoom and endRoom

--- utils/socketIOMiddlewares.js
+++ utils/socketIOMiddlewares.js
@@ -160,7 +160,10 @@ exports.socketIOHandler = function (io) {
                 }
 
                 if (roomName) {
-                    const existingRoom = await Room.findOne({ name: roomName })
+                    const existingRoom = await Room.findOne({
+                        name: roomName,
+                        isActivated: true,
+                    })
 
                     if (!existingRoom) {
                         io.to(socket.id).emit('errorMessage', 'room not found')
@@ -499,8 +502,9 @@ exports.socketIOHandler = function (io) {
                     io.to(existingRoom.name).emit('roomEnded')
                     io.in(socket.user.roomName).disconnectSockets(true)
                     try {
-                        const foundRoom = await Room.findOneAndDelete({
+                        await Room.updateOne({
                             name: socket.user.roomName,
+                            isActivated: false,
                         })
                     } catch (err) {
                         console.log(err)
